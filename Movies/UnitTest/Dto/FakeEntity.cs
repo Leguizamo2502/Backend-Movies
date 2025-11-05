9f09a9243e7149e742fb9507ff3a6ed430c83704
Sergio <vargasleguizamo95@gmail.com>
2025-11-04 23:36:56 -0500

    pruebas

---
+++ Movies/UnitTest/Dto/FakeEntity.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Domain.Models.Base;
+
+namespace UnitTest.Dto
+{
+    public class FakeEntity : BaseModel
+    {
+        public string? Name { get; set; }
+    }
+
+    public class FakeCreateDto
+    {
+        public string? Name { get; set; }
+    }
+    public class FakeUpdate
+    {
+        public string? Name { get; set; }
+    }
+
+    public class FakeSelectDto
+    {
+        public string? Name { get; set; }
+    }
+}
