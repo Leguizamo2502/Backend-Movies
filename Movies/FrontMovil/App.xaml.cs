3a21927059562c0980af27ef593adbfa7188a028
Sergio <vargasleguizamo95@gmail.com>
2025-11-09 11:43:46 -0500

    Estructira maui

---
+++ Movies/FrontMovil/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontMovil
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
